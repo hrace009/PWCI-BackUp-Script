6d551b0aa8c97cc418125f097257b7b86807d986
hrace009 <hrace009@gmail.com>
2025-04-21 11:09:43 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -24,20 +24,20 @@
 #
 
 ## set default variables
-backup_root=/backup			                                  # root directory for all backups
-v=true					                                          # verbose output
-keep=5					                                          # number of old backups to keep
-hash=sha256				                                        # crypto hash to use for checksums
+backup_root=/backup			                  # root directory for all backups
+v=true					                  # verbose output
+keep=5					                  # number of old backups to keep
+hash=sha256				                  # crypto hash to use for checksums
 costumfilename=PWCI                                       # backup file name
 target_backup=/home                                       # backup target
 target_backup2=/root                                      # backup target 2
 pwlogs_folder=/home/logs                                  # pw logs folder
 weblogs_folder=/var/logs/nginx                            # website logs folder
-webconfigs=/etc/nginx						                          # Website Configuration
-webdata=/var/www						                              # Website Data
+webconfigs=/etc/nginx					  # Website Configuration
+webdata=/var/www					  # Website Data
 mysqlconf=/etc/my.cnf.d                                   # Mysql Config
 gdrive_folder=PWCI_155                                    # google drive folder ID
-phpfpm=/etc/php-fpm.d				                              # PHP FPM CONFIG
+phpfpm=/etc/php-fpm.d				          # PHP FPM CONFIG
 
 # set mysqldump options
 dumpopts='--single-transaction --flush-logs --flush-privileges --routines  --triggers'
